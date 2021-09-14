02118861093ad98d958fc88c2bb313002f04898c
hari-testpress <hari@testpress.in>
2021-09-14 17:31:39 +0530

    Run development server throught HTTPS

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
