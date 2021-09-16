4c5e1efe58cae88e1249e4d330a2f730b8d9b000
hari-testpress <hari@testpress.in>
2021-09-16 15:09:34 +0530

    Create detail view for image

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Image
 
 # Create your views here.
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image},
+    )
