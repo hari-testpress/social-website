faef339846b447a66c5adc446ecdbb5762a975b2
hari-testpress <hari@testpress.in>
2021-09-16 07:42:30 +0530

    Add bookmarklet launcher

---
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -0,0 +1,10 @@
+(function(){
+    if (window.myBookmarklet !== undefined){
+        myBookmarklet();
+    }
+    else {
+        var script = document.createElement('script')
+        script.src='https://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999)
+        document.body.appendChild(script)
+    }
+})();
