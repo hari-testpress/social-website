2bfd31d9e33231081f195bfe2df65ac5f7dcc8a3
hari-testpress <hari@testpress.in>
2021-09-16 11:36:11 +0530

    Create a bookmarklet with JQuery

--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -3,8 +3,6 @@
         myBookmarklet();
     }
     else {
-        var script = document.createElement('script')
-        script.src='https://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999)
-        document.body.appendChild(script)
+        document.body.appendChild(document.createElement('script')).src='https://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
     }
 })();
