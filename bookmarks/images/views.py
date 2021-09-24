963e7798dea2a39d2b602b597f4b123e4e0e183e
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-09-24 14:05:08 +0530

    Add support to store user activity in the action model (#28)

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404
 from common.decorators import ajax_required
 from .models import Image
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
 
 
 @login_required
@@ -19,6 +20,7 @@ def image_create(request):
             new_item = form.save(commit=False)
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, "bookmarked image", new_item)
             messages.success(request, "Image added successfully")
 
             return redirect(new_item.get_absolute_url())
@@ -78,6 +80,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == "like":
                 image.users_like.add(request.user)
+                create_action(request.user, "likes", image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({"status": "ok"})
